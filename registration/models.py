f5e5a94f4a623b6fcdf3dae480a543973a881326
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-06-11 10:10:10 +0300

    migrate bd one

--- registration/models.py
+++ registration/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-class User(model.Models):
-    id_user = models.ForeignKey()
+class User(models.Model):
     name = models.CharField("Имя пользователя", max_length=50)
     email = models.CharField("Почта", max_length=50)
     phone = models.CharField("Телефон", max_length=20)
 
 class Shop(models.Model):
-    id_shop = models.ForeignKey()
     name = models.CharField("Имя продавца", max_length=50)
     registration_number = models.CharField("Регистрационный номер", max_length=50)
     registration_date = models.DateField("Дата регистрации")
@@ -20,5 +18,5 @@ class Shop(models.Model):
 class Client(models.Model):
     login = models.CharField("Имя продавца", max_length=50)
     password = models.CharField("Имя продавца", max_length=50)
-    id_shop = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
-    id_user = models.OneToOneField(Shop, on_delete = models.CASCADE, primary_key = True) 
+    id_shop = models.OneToOneField(User, on_delete = models.CASCADE)
+    id_user = models.OneToOneField(Shop, on_delete = models.CASCADE) 
