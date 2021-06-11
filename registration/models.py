7cdf7d94057a5eb56dd4472a9ac736bd66c3b056
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-06-11 10:00:41 +0300

    models_bd

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(model.Models):
+    id_user = models.ForeignKey()
+    name = models.CharField("Имя пользователя", max_length=50)
+    email = models.CharField("Почта", max_length=50)
+    phone = models.CharField("Телефон", max_length=20)
+
+class Shop(models.Model):
+    id_shop = models.ForeignKey()
+    name = models.CharField("Имя продавца", max_length=50)
+    registration_number = models.CharField("Регистрационный номер", max_length=50)
+    registration_date = models.DateField("Дата регистрации")
+    physical_adress = models.CharField("Физический адресс", max_length=200)
+    lint = models.CharField("?", max_length=50)
+    organization_person = models.CharField("Имя организации", max_length=50)
+    email = models.CharField("Почта продавца", max_length=50)
+    phone = models.CharField("Телефон продавца", max_length=50)
+
+class Client(models.Model):
+    login = models.CharField("Имя продавца", max_length=50)
+    password = models.CharField("Имя продавца", max_length=50)
+    id_shop = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    id_user = models.OneToOneField(Shop, on_delete = models.CASCADE, primary_key = True) 
