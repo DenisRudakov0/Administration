a18b20b62f2727aaa685fc9ecc3b8aece0084bd4
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-06-12 17:36:28 +0300

    modification views and models

--- registration/models.py
+++ registration/models.py
@@ -3,17 +3,21 @@ from django.db import models
 class Client(models.Model):
     login = models.CharField("Имя продавца", max_length=50)
     password = models.CharField("Имя продавца", max_length=50)
-    id_shop = models.IntegerField()
-    id_user = models.IntegerField() 
-    
+    user = models.OneToOneField('User', on_delete = models.CASCADE, null=True)
+    shop = models.OneToOneField('Shop', on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.login
+
 class User(models.Model):
-    id = models.OneToOneField(Client, on_delete = models.CASCADE, primary_key = True)
     name = models.CharField("Имя пользователя", max_length=50)
     email = models.CharField("Почта", max_length=50)
     phone = models.CharField("Телефон", max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Shop(models.Model):
-    id = models.OneToOneField(Client, on_delete = models.CASCADE, primary_key = True)
     name = models.CharField("Имя продавца", max_length=50)
     registration_number = models.CharField("Регистрационный номер", max_length=50)
     registration_date = models.DateField("Дата регистрации")
@@ -22,5 +26,8 @@ class Shop(models.Model):
     organization_person = models.CharField("Имя организации", max_length=50)
     email = models.CharField("Почта продавца", max_length=50)
     phone = models.CharField("Телефон продавца", max_length=50)
+
+    def __str__(self):
+        return self.name   
 
 
--- registration/views.py
+++ registration/views.py
@@ -1,15 +1,26 @@
 from os import name
 from django import http
+from django.db.models.fields import AutoField, Field
+from django.db.models.fields.related import OneToOneField
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from .models import User, Client, Shop
 import sqlite3
 import datetime
 
-
 def create(request):
+    """   
     data = {
         'login': 'petr@mail.ru',
+        'password': '123456',
+        'role': 'user',
+        'name': 'Ivan',
+        'email': 'petr@mail.ru',
+        'phone': '+243214566675'
+    }
+    """  
+    data = {
+        'login': 'ivanov@gmail.com',
         'password': '123456',
         'role': 'shop',
         'name': 'Ivan',
@@ -21,32 +32,34 @@ def create(request):
         'email': 'petr@mail.ru',
         'phone': '+243214566675'
     }
-
-    Client.objects.all().delete()
-    Shop.objects.all().delete()
-    User.objects.all().delete()
-
+    
     len_table = len(Client.objects.all()) + 1
     table_client = Client.objects.all()
     table_user = User.objects.all()
     table_shop = Shop.objects.all()
 
- 
     if data['role'] == 'user':
-        table_user.create(name = data['name'], email = data['email'], phone = data['phone'])
-        table_shop.create()
+        id_user = create_user(request, data, table_user)
+        Client.objects.create(login = data['login'], password = data['password'], user = User.objects.get(pk=id_user))
     elif data['role'] == 'shop':
-        date_registration = data['registration_date'].split('.')
-        table_shop.create(name = data['name'], registration_number = data['registration_number'],
-                          registration_date = datetime.date(*[int(i) for i in date_registration[::-1]]), physical_adress = data['physical_adress'],
-                          lint = data['lint'], organization_person = data['organization_person'],
-                          email = data['email'], phone = data['phone'])
-        table_user.create()
+        id_shop = create_shop(request, data, table_shop)
+        Client.objects.create(login = data['login'], password = data['password'], shop = Shop.objects.get(pk=id_shop))
     else:
         print('Упс! Вылезла какае-то ошибочка :)')
-    table_client.create(login = data['login'], password = data['password'], 
-                        id_shop = Shop.objects.id(), id_user = len_table)
 
     return HttpResponse(1)
 
+def create_shop(request, data, table_shop):
+    date_registration = data['registration_date'].split('.')
+    w = Shop.objects.create(name = data['name'], registration_number = data['registration_number'],
+                          registration_date = datetime.date(*[int(i) for i in date_registration[::-1]]),
+                          physical_adress = data['physical_adress'],
+                          lint = data['lint'], organization_person = data['organization_person'],
+                          email = data['email'], phone = data['phone'])
+    return w.id
+    
+
+def create_user(request, data, table_user):
+    w = table_user.create(name = data['name'], email = data['email'], phone = data['phone'])
+    return w.id
 # Create your views here.
