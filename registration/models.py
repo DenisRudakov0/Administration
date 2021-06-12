3ac72f49ba7e2895ff567d22ab6286be2171943e
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-06-12 09:20:47 +0300

    Customizaztion urls, write views, new_migrate

--- registration/models.py
+++ registration/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+class Client(models.Model):
+    login = models.CharField("Имя продавца", max_length=50)
+    password = models.CharField("Имя продавца", max_length=50)
+    id_shop = models.IntegerField()
+    id_user = models.IntegerField() 
+    
 class User(models.Model):
+    id = models.OneToOneField(Client, on_delete = models.CASCADE, primary_key = True)
     name = models.CharField("Имя пользователя", max_length=50)
     email = models.CharField("Почта", max_length=50)
     phone = models.CharField("Телефон", max_length=20)
 
 class Shop(models.Model):
+    id = models.OneToOneField(Client, on_delete = models.CASCADE, primary_key = True)
     name = models.CharField("Имя продавца", max_length=50)
     registration_number = models.CharField("Регистрационный номер", max_length=50)
     registration_date = models.DateField("Дата регистрации")
@@ -15,8 +23,4 @@ class Shop(models.Model):
     email = models.CharField("Почта продавца", max_length=50)
     phone = models.CharField("Телефон продавца", max_length=50)
 
-class Client(models.Model):
-    login = models.CharField("Имя продавца", max_length=50)
-    password = models.CharField("Имя продавца", max_length=50)
-    id_shop = models.OneToOneField(User, on_delete = models.CASCADE)
-    id_user = models.OneToOneField(Shop, on_delete = models.CASCADE) 
+
--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,52 @@
+from os import name
 from django import http
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from .models import User, Client, Shop
 import sqlite3
+import datetime
 
 
-def index(request):
-    return HttpResponse("Hello, World")
+def create(request):
+    data = {
+        'login': 'petr@mail.ru',
+        'password': '123456',
+        'role': 'shop',
+        'name': 'Ivan',
+        'registration_number': '21312412412412',
+        'registration_date': '12.02.2020',
+        'physical_adress': 'ул. Пушкинская, д - 8',
+        'lint': 'asasdadad',
+        'organization_person': 'Пупкин Василий Иванович',
+        'email': 'petr@mail.ru',
+        'phone': '+243214566675'
+    }
+
+    Client.objects.all().delete()
+    Shop.objects.all().delete()
+    User.objects.all().delete()
+
+    len_table = len(Client.objects.all()) + 1
+    table_client = Client.objects.all()
+    table_user = User.objects.all()
+    table_shop = Shop.objects.all()
+
+ 
+    if data['role'] == 'user':
+        table_user.create(name = data['name'], email = data['email'], phone = data['phone'])
+        table_shop.create()
+    elif data['role'] == 'shop':
+        date_registration = data['registration_date'].split('.')
+        table_shop.create(name = data['name'], registration_number = data['registration_number'],
+                          registration_date = datetime.date(*[int(i) for i in date_registration[::-1]]), physical_adress = data['physical_adress'],
+                          lint = data['lint'], organization_person = data['organization_person'],
+                          email = data['email'], phone = data['phone'])
+        table_user.create()
+    else:
+        print('Упс! Вылезла какае-то ошибочка :)')
+    table_client.create(login = data['login'], password = data['password'], 
+                        id_shop = Shop.objects.id(), id_user = len_table)
+
+    return HttpResponse(1)
 
 # Create your views here.
