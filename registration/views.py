b90faa85162cb247f6a494321bb058aa28d6e18b
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-06-12 06:31:31 +0300

    Customizaztion urls

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,14 @@
+from django import http
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect
+from .models import User, Client, Shop
+import sqlite3
+
+
+def index(request):
+    try:
+        print('OK')
+    except:
+        raise Http404('Ошибка внесения данных в БД')
 
 # Create your views here.
