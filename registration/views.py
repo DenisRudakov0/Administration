fd7599023f2a0fb47b6963f5d30d529004729097
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-06-30 11:01:04 +0300

    add_hash_password

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,6 @@
 from os import name
+import os
+import hashlib
 from django import http
 from django.db.models.fields import AutoField, Field
 from django.db.models.fields.related import OneToOneField
@@ -16,12 +18,19 @@ def create(request):
     try:
         if Client.objects.filter(login = data['login']).exists() == True:
             return HttpResponse('Пользователь с данным логином уже зарегистрирован')
+        else:
+            salt = os.urandom(32)
+            password = data['password']
+
+            key = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000, dklen = 128)
+            password = salt + key
+
         if data['role'] == 'user':
             id_user = create_user(request, data)
-            Client.objects.create(login = data['login'], password = data['password'], user = User.objects.get(pk=id_user))
+            Client.objects.create(login = data['login'], password = password, user = User.objects.get(pk=id_user))
         elif data['role'] == 'shop':
             id_shop = create_shop(request, data)
-            Client.objects.create(login = data['login'], password = data['password'], shop = Shop.objects.get(pk=id_shop))
+            Client.objects.create(login = data['login'], password = password, shop = Shop.objects.get(pk=id_shop))
         else:
             return HttpResponse('Упс! Возникла ошибочка при определении роли')
         return HttpResponse('Данные успешно добавлены :)')
